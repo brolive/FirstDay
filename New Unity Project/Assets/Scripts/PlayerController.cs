bf03e3ccc700027db1eaaa4c62d6fd029bcdc561
brolive <brolive@my.lonestar.edu>
2019-09-06 12:03:27 -0500

    FIRST

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float jumpForce = 1.0f;
+
+    private bool grounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("TEST!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position,
+                           Vector3.down,
+                           2))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Debug.Log(horizontalInput);
+
+        /*transform.position = new Vector3(transform.position.x + horizontalInput,
+                                         transform.position.y,
+                                           transform.position.z);*/
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.velocity = new Vector3(horizontalInput * speed,
+                                  rb.velocity.y,
+                                  verticalInput);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x,
+                                      jumpForce,
+                                      rb.velocity.z);
+        }
+    }
+}
